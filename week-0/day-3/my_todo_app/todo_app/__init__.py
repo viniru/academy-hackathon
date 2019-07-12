7bc8e9166691b0c5c5e762afe7581115a4e44313
vinayak bhat <viniru100@gmail.com>
2019-07-13 05:09:50 +0530

    added MVC Model

--- week-0/day-3/my_todo_app/todo_app/__init__.py
+++ week-0/day-3/my_todo_app/todo_app/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask import request
 
 
 def create_app(test_config=None):
@@ -13,13 +14,39 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that list my todos
-    @app.route('/shivang')
-    def shivang():
-        return ('Wake Up' + '<br/>' +
-            'Drink Coffee' + '<br/>' +
-            'Read Non-fiction Novel' + '<br/>'
-        )
+    def todo_view(todos):
+        the_view = 'List of my todos:' + '<br/>'
+        for todo in todos:
+            the_view += ( todo + '<br/>' )
+
+        the_view += '---- LIST ENDS HERE ---'
+        return the_view
+
+    def get_todos_by_name(name):
+        if name == 'depo':
+            return ['Go for run', 'Listen Rock Music']
+        elif name == 'shivang':
+            return ['Read book', 'Play Fifa', 'Drink Coffee']
+        elif name == 'raj':
+            return ['Study', 'Brush']
+        elif name == 'sanket':
+            return ['Sleep', 'Code']
+        elif name == 'aagam':
+            return ['play cricket', 'have tea']
+        else:
+            return []
+
+
+    # http://127.0.0.1:5000/todos?name=duster
+    @app.route('/todos')
+    def todos():
+        name = request.args.get('name')
+        print('---------')
+        print(name)
+        print('---------')
+
+        person_todo_list = get_todos_by_name(name)
+        return todo_view(person_todo_list)
 
     return app
 
